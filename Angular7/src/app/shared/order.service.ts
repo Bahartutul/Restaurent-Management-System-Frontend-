8583b1678030d9e144dd0bbf64577373cbc94dd8
bahartutul <tutulaiub@gmail.com>
2019-12-22 02:41:46 +0600

    getpostToken

--- Angular7/src/app/shared/order.service.ts
+++ Angular7/src/app/shared/order.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Order } from './order.model';
 import { OrderItem } from './order-item.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -14,12 +14,14 @@ orderItems:OrderItem[];
   ngOnInit(){
 
   }
+  token=localStorage.getItem('access_token');
+  headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.token });
   postAllOrder(){
     var body={
       ...this.formData,
       OrderItems:this.orderItems
     }
-   return this.http.post(environment.apiUrl+"/Orders",body);
+   return this.http.post(environment.apiUrl+"/Orders",body,{headers:this.headers});
   }
   getOrderById(id:number):any{
   return this.http.get(environment.apiUrl+"/Orders/"+id).toPromise();
